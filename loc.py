f60200f5d4c4467afb94ddadaee790d7b8a19f0a
EuSuntAlex <orac_alex@yahoo.com>
2025-02-12 13:24:42 +0200

    added EasyMode implementation but it's kinda baddddddd

---
+++ loc.py
@@ -0,0 +1,14 @@
+locations = [
+    {
+        "name": "Safi P3",
+        "images": ["safip3_1.jpg", "safip3_2.jpg", "safip3_3.jpg"],
+        "options": ["Ancient Forest", "Wildspire Waste", "Coral Highlands", "Rotten Vale", "Elder's Recess", "Safi Pit"],
+        "correct": "Safi Pit"
+    },
+    {
+        "name": "Gather",
+        "images": ["gather_1.jpg", "gather_2.jpg", "gather_3.jpg"],
+        "options": ["Gathering Hall", "Ancient Forest", "Coral Highlands", "Rotten Vale", "Elder's Recess", "Hoarfrost Reach"],
+        "correct": "Gathering Hall"
+    }
+]
