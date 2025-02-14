31bd8d931a7cb942484267eb262d9cd755f894e4
EuSuntAlex <orac_alex@yahoo.com>
2025-02-14 23:05:31 +0200

    added photos and coordinates

--- pinpoint.py
+++ pinpoint.py
@@ -46,6 +46,148 @@ points = [
         "correct": (964, 492)
     },
     {
+        "images": "hf1.jpg",
+        "location": "Hoarfrost Reach Platform 1", 
+        "correct": (1110, 301)
+    },
+    {
+        "images": "hf2.jpg",
+        "location": "Hoarfrost Reach Platform 2", 
+        "correct": (912, 479)
+    },
+    {
+        "images": "hf3.jpg",
+        "location": "Hoarfrost Reach Platform 2", 
+        "correct": (953, 845)
+    },
+    {
+        "images": "hf4.jpg",
+        "location": "Hoarfrost Reach Platform 2", 
+        "correct": (732, 745)
+    },
+    {
+        "images": "hf5.jpg",
+        "location": "Hoarfrost Reach Platform 1", 
+        "correct": (981, 587)
+    },
+    {
+        "images": "hf6.jpg",
+        "location": "Hoarfrost Reach Platform 1", 
+        "correct": (1047, 431)
+    },
+    {
+        "images": "hf7.jpg",
+        "location": "Hoarfrost Reach Platform 1", 
+        "correct": (1045, 355)
+    },
+    {
+        "images": "hf8.jpg",
+        "location": "Hoarfrost Reach Platform 2", 
+        "correct": (1125, 477)
+    },
+    {
+        "images": "hf9.jpg",
+        "location": "Hoarfrost Reach Platform 2", 
+        "correct": (868, 529)
+    },
+    {
+        "images": "hf10.jpg",
+        "location": "Hoarfrost Reach Platform 2", 
+        "correct": (768, 457)
+    },
+    {
+        "images": "hf11.jpg",
+        "location": "Hoarfrost Reach Platform 2", 
+        "correct": (768, 457)
+    },
+    {
+        "images": "hf12.jpg",
+        "location": "Hoarfrost Reach Platform 2", 
+        "correct": (768, 457)
+    },
+    {
+        "images": "hf13.jpg",
+        "location": "Hoarfrost Reach Platform 2", 
+        "correct": (874, 376)
+    },
+    {
+        "images": "hf14.jpg",
+        "location": "Hoarfrost Reach Platform 2", 
+        "correct": (999, 324)
+    },
+
+
+    {
+        "images": "rv1.jpg",
+        "location": "Rotten Vale Platform 3", 
+        "correct": (1104, 727)
+    },
+    {
+        "images": "rv2.jpg",
+        "location": "Rotten Vale Platform 3", 
+        "correct": (896, 651)
+    },
+    {
+        "images": "rv3.jpg",
+        "location": "Rotten Vale Platform 3", 
+        "correct": (829, 635)
+    },
+    {
+        "images": "rv4.jpg",
+        "location": "Rotten Vale Platform 2", 
+        "correct": (1128, 455)
+    },
+    {
+        "images": "rv5.jpg",
+        "location": "Rotten Vale Platform 2", 
+        "correct": (1171, 411)
+    },
+    {
+        "images": "rv6.jpg",
+        "location": "Rotten Vale Platform 2", 
+        "correct": (1191, 315)
+    },
+    {
+        "images": "rv7.jpg",
+        "location": "Rotten Vale Platform 2", 
+        "correct": (1086, 579)
+    },
+    {
+        "images": "rv8.jpg",
+        "location": "Rotten Vale Platform 2", 
+        "correct": (1053, 610)
+    },
+    {
+        "images": "rv9.jpg",
+        "location": "Rotten Vale Platform 2", 
+        "correct": (999, 488)
+    },
+    {
+        "images": "rv10.jpg",
+        "location": "Rotten Vale Platform 2", 
+        "correct": (944, 354)
+    },
+    {
+        "images": "rv11.jpg",
+        "location": "Rotten Vale Platform 1", 
+        "correct": (848, 424)
+    },
+    {
+        "images": "rv12.jpg",
+        "location": "Rotten Vale Platform 1", 
+        "correct": (848, 392)
+    },
+    {
+        "images": "rv13.jpg",
+        "location": "Rotten Vale Platform 1", 
+        "correct": (744, 324)
+    },
+    {
+        "images": "rv14.jpg",
+        "location": "Rotten Vale Platform 1", 
+        "correct": (687, 263)
+    },
+    {
         "images": "Rotten_Vale_02.jpg",
         "location": "Rotten Vale Platform 1", 
         "correct": (1023, 434)
@@ -55,5 +197,6 @@ points = [
         "location": "Rotten Vale Platform 1", 
         "correct": (562, 517)
     },
+    
 
 ]
